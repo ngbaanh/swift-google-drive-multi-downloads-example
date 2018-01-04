ecc014a3cba99852203e0408e750239db6d96bdd
Bá Anh Nguyễn <baanh.nguyen@outlook.com>
2018-01-04 11:15:48 +0700

    Initial Commit

---
+++ GoogleDriveDownloaderTests/GoogleDriveDownloaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoogleDriveDownloaderTests.swift
+//  GoogleDriveDownloaderTests
+//
+//  Created by Bá Anh Nguyễn on 1/4/18.
+//  Copyright © 2018 Bá Anh Nguyễn. All rights reserved.
+//
+
+import XCTest
+@testable import GoogleDriveDownloader
+
+class GoogleDriveDownloaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
